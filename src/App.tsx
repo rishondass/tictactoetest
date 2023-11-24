d8bd60ea95a5767ff2810791a994d610e2c3a1a0
Rishon Dass <rishondass@gmail.com>
2023-11-24 06:46:16 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="lobby" element={<Lobby />} />
+          <Route path="/lobby" element={<Lobby />} />
         </Route>
       </Routes>
     </AuthWrapper>
