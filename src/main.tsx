5e77fb3a306c7215f309950bf67f68d9c6856db1
Rishon Dass <rishondass@gmail.com>
2023-11-24 06:43:05 -0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tictactoetest">
     <Routes>
-      <Route path="/tictactoetest/*" element={<App />}/>
+      <Route path="/*" element={<App />}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
